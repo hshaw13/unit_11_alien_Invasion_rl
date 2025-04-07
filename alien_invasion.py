ac5a47ca5b04cdd363fd91a7fb1b3106f9dc610a
hshaw13 <hamadyshaw10@gmail.com>
2025-04-06 22:23:23 -0400

    created a ship and installed pygame

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,36 @@
 import sys
 import pygame
 
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
+
+    ship = ship(screen)
+
+    # Main loop
+    while True:
+        # Event loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    ship.moving_up = True
+                elif event.key == pygame.K_DOWN:
+                    ship.moving_down = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP:
+                    ship.moving_up = False
+                elif event.key == pygame.K_DOWN:
+                    ship.moving_down = False
+
+        # Update ship
+        ship.update()
+
+        # Redraw screen
+        screen.fill((0, 0, 0))
+        ship.blitme()
+        pygame.display.flip()
 if __name__ == '__main__':
     pass
